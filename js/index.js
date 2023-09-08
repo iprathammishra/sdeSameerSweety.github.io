5d5452f2dfeae9a1ce19d2fd39f759fdb86f8d0d
Pratham <mishrapratham.codeit@gmail.com>
2023-09-08 17:55:04 +0000

    fix: fixed the active navigation logic

--- js/index.js
+++ js/index.js
@@ -7,4 +7,50 @@ var typed = new Typed(".typing",{
     loop:true
 })
 
+let tCol = document.getElementsByClassName('navi')
 
+document.addEventListener('mousemove', () => {
+    if (document.URL.endsWith("#home")) {
+        tCol[0].style.color = 'red';
+        tCol[1].style.color = '#302e4d'
+        tCol[2].style.color = '#302e4d'
+        tCol[3].style.color = '#302e4d'
+        tCol[4].style.color = '#302e4d'
+        tCol[5].style.color = '#302e4d'
+    } else if (document.URL.endsWith("#about")) {
+        tCol[0].style.color = '#302e4d';
+        tCol[1].style.color = 'red'
+        tCol[2].style.color = '#302e4d'
+        tCol[3].style.color = '#302e4d'
+        tCol[4].style.color = '#302e4d'
+        tCol[5].style.color = '#302e4d'
+    } else if (document.URL.endsWith("#achievement")) {
+        tCol[0].style.color = '#302e4d';
+        tCol[1].style.color = '#302e4d'
+        tCol[2].style.color = 'red'
+        tCol[3].style.color = '#302e4d'
+        tCol[4].style.color = '#302e4d'
+        tCol[5].style.color = '#302e4d'
+    } else if (document.URL.endsWith("#education")) {
+        tCol[0].style.color = '#302e4d';
+        tCol[1].style.color = '#302e4d'
+        tCol[2].style.color = '#302e4d'
+        tCol[3].style.color = 'red'
+        tCol[4].style.color = '#302e4d'
+        tCol[5].style.color = '#302e4d'
+    } else if (document.URL.endsWith("portfolio")) {
+        tCol[0].style.color = '#302e4d';
+        tCol[1].style.color = '#302e4d'
+        tCol[2].style.color = '#302e4d'
+        tCol[3].style.color = '#302e4d'
+        tCol[4].style.color = 'red'
+        tCol[5].style.color = '#302e4d'
+    } else if (document.URL.endsWith("contact")) {
+        tCol[0].style.color = '#302e4d';
+        tCol[1].style.color = '#302e4d'
+        tCol[2].style.color = '#302e4d'
+        tCol[3].style.color = '#302e4d'
+        tCol[4].style.color = '#302e4d'
+        tCol[5].style.color = 'red'
+    }
+});
